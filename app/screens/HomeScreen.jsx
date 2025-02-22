00dc3ddca5e4bb858e6af4ff8e78ff419ab4effe
XplendevGito <l.riveraa664@hotmail.com>
2025-02-22 20:59:49 -0300

    UpdateNameApp

--- app/screens/HomeScreen.jsx
+++ app/screens/HomeScreen.jsx
@@ -5,7 +5,7 @@ export default function HomeScreen() {
     <View className="flex-1 justify-center items-center bg-gray-100">
       <Text className="text-2xl font-bold text-blue-600 mb-4">🏠 Coffee Time App</Text>
       <TouchableOpacity className="bg-blue-500 p-4 rounded-lg">
-        <Text className="text-white">Comenzemos a crear!!</Text>
+        <Text className="text-white">Comenzemos a crearssss!!</Text>
       </TouchableOpacity>
     </View>
   );
