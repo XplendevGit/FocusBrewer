888bd636dc8d04b00c0c6af90ecb217a1b5b066c
XplendevGito <l.riveraa664@hotmail.com>
2025-04-06 02:12:22 -0400

    updates v0.02

---
+++ app/screens/ConcentrationScreen.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ConcentrationScreen = () => {
+  return (
+    <div>ConcentrationScreen</div>
+  )
+}
+
+export default ConcentrationScreen
--- app/screens/HomeScreen.jsx
+++ app/screens/HomeScreen.jsx
@@ -3,9 +3,10 @@ import { View, Text, TouchableOpacity } from 'react-native';
 export default function HomeScreen() {
   return (
     <View className="flex-1 justify-center items-center bg-gray-100">
-      <Text className="text-2xl font-bold text-blue-600 mb-4">🏠 Coffee Time App</Text>
-      <TouchableOpacity className="bg-blue-500 p-4 rounded-lg">
-        <Text className="text-white">Comenzemos a crearssss!!</Text>
+      <Text className="text-2xl font-bold text-blue-600 mb-4">FocusCoffee App Hello World</Text>
+      <TouchableOpacity 
+          className="bg-blue-500 p-4 rounded-lg">
+        <Text className="text-white">Comenzemos a crear compa dele nomas!!</Text>
       </TouchableOpacity>
     </View>
   );
